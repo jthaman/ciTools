10fedf559eea1d4552deb3cef7d7aa5968266992
John Haman <jhaman@ida.org>
2017-06-20 17:07:28 -0400

    fixed my add quantile lm function
    I was flat wrong when coding this originally

--- add_probs_lm.R
+++ add_probs_lm.R
@@ -1,1 +1,21 @@
-## TODO : add_probs method for lm objects
+## add_probs method for lm objects
+add_probs.lm <- function(tb, fit, quantile = 0.20, probName = NULL){
+    if (quant <= 0 || quant >= 1)
+        stop ("quant should be in (0,1)")
+    if (is.null(quantileName))
+        quantileNameCheck <- paste(quant, "-Quantile", sep="")
+    if (!(quantileNameCheck %in% colnames(tb)))
+        quantileName <- quantileNameCheck
+    out <- predict(fit, tb, interval = "prediction", se.fit = TRUE)
+    fitted <- out$fit[,1]
+    residual_df <- out$df
+    se_fitted <- out$se.fit
+    t_quantile <- qt(p = quant, df = residual_df)
+    out_quantiles <- fitted + se_fitted * t_quantile
+    if (is.null(tb[["pred"]])) tb[["pred"]] <- fitted
+    tb[[quantileName]] <- out_quantiles
+    tb
+    
+}
+
+
--- add_quantile_lm.R
+++ add_quantile_lm.R
@@ -1,20 +1,23 @@
 ## add_quantile method for lm objects
 
-add_quantile.lm <- function(tb, fit, prob = 0.20, quantileName = NULL){
+add_quantile.lm <- function(tb, fit, prob, quantileName = NULL){
     if (prob == 0.5)
         warning ("The 0.5 quantile is equal to the fitted values")
     if (prob <= 0 || prob >= 1)
         stop ("prob should be in (0,1)")
     if (is.null(quantileName))
-        quantileNameCheck <- paste(prob, "-quantile", sep="")
+        quantileNameCheck <- paste("quantile", prob, sep="")
     if (!(quantileNameCheck %in% colnames(tb)))
         quantileName <- quantileNameCheck
+    n <- NROW(tb)
     out <- predict(fit, tb, interval = "prediction", se.fit = TRUE)
     fitted <- out$fit[,1]
     residual_df <- out$df
     se_fitted <- out$se.fit
+    resid_var <- out$residual.scale^2
+    se_pred <- sqrt(resid_var + se_fitted^2)
     t_quantile <- qt(p = prob, df = residual_df)
-    out_quantiles <- fitted + se_fitted * t_quantile
+    out_quantiles <- fitted + se_pred * t_quantile
     if (is.null(tb[["pred"]])) tb[["pred"]] <- fitted
     tb[[quantileName]] <- out_quantiles
     return(tb)
