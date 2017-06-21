583d8f4689218a270f0e23a2677822be25661675
John Haman <jhaman@ida.org>
2017-06-21 15:41:47 -0400

    Merged in Matt's edits to add_ci GLM method

--- add_ci_glm.R
+++ add_ci_glm.R
@@ -1,12 +1,12 @@
-#add_ci method for GLMs
-## TODO : add Matt's edits
+## add_ci method for GLMs
 ## TODO : add Bootstrap method (not a primary concern)
 
 add_ci.glm <- function(tb, fit, alpha = 0.05, ciNames = c("LCB", "UCB"),
                        type = "response", ciType = "parametric", bootReplicates = 1000){
 
   if(ciType == "bootstrap") return(
-    bootstrap_ci_glm(tb, fit, alpha, ciNames, type, bootReplicates, ...)
+                                stop("not yet implemented")
+    ##bootstrap_ci_glm(tb, fit, alpha, ciNames, type, bootReplicates, ...)
   ) else
     if(ciType == "parametric") return(
       parametric_ci_glm(tb, fit, alpha, ciNames, type,)
@@ -15,25 +15,44 @@ add_ci.glm <- function(tb, fit, alpha = 0.05, ciNames = c("LCB", "UCB"),
 
 }
 
-## add_ci Wald method for glm
+parametric_ci_glm <- function(tb, fit, alpha, ciNames, type = "response"){
 
-parametric_ci_glm <- function(tb, fit, alpha, ciNames, type, ...){
-    out <- predict(fit, tb, se.fit = TRUE)
-    crit_val <- qt(p = 1 - alpha/2, df = fit$df.residual)
-    inverselink <- fit$family$linkinv
-    if(type == "response"){
-        upr <- inverselink(out$fit + crit_val * out$se.fit)
-        lwr <- inverselink(out$fit - crit_val * out$se.fit)
-        pred <- inverselink(out$fit)
-    }else{
-        upr <- out$fit + crit_val * out$se.fit
-        lwr <- out$fit - crit_val * out$se.fit
-        pred <- out$fit
-    }
-    if(is.null(tb[["pred"]])) tb[["pred"]] <- pred
-    tb[[ciNames[1]]] <- lwr
-    tb[[ciNames[2]]] <- upr
-    tb
+  inverselink <- fit$family$linkinv
+  if(type == "response"){
+    out <- glm_ci_response(tb, fit, alpha, inverselink) %>%
+      plyr::rename(c("lwr" = ciNames[1], "upr" = ciNames[2])) %>%
+      select(-.se)
+  }
+
+  if(type == "link"){
+    out <- glm_ci_linear_predictor(tb, fit, alpha) %>%
+      plyr::rename(c("lwr" = ciNames[1], "upr" = ciNames[2])) %>%
+      select(-.se)
+  }
+  if(!(type %in% c("response", "link"))) top("Incorrect interval type specified!")
+  out
+}
+
+glm_ci_response <- function(tb, fit, alpha, ilink){
+
+  if(is.null(tb[["pred"]])) tb <- modelr::add_predictions(tb, fit)
+  tb %>%
+    add_standard_error(fit) %>%
+    mutate(
+      lwr = ilink(pred + qt(alpha/2, df = fit$df.residual) * .se),
+      upr = ilink(pred + qt(1 - alpha/2, df = fit$df.residual) * .se)
+    )
 
 }
 
+glm_ci_linear_predictor <- function(tb, fit, alpha){
+
+  if(is.null(tb[["pred"]])) tb <- modelr::add_predictions(tb, fit)
+  tb %>%
+    add_standard_error(fit) %>%
+    mutate(
+      lwr = pred + qt(alpha/2, df = fit$df.residual) * .se,
+      upr = pred + qt(1 - alpha/2, df = fit$df.residual) * .se
+    )
+
+}
