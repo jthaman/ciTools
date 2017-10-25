154b03ef075ca2cbb07e8b13b86f66e62f6c474f
John Haman <mail@johnhaman.org>
2017-10-24 21:19:02 -0400

    add_pi method for GLMMs (draft)

--- R/add_pi_glmer.R
+++ R/add_pi_glmer.R
@@ -15,13 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with ciTools. If not, see <http://www.gnu.org/licenses/>.
 
-#' Confidence Intervals for Generalized Linear Mixed Model Predictions
+#' Prediction Intervals for Generalized Linear Mixed Model Predictions
 #'
-#' This function is one of the methods for \code{add_ci}, and is
-#' called automatically when \code{add_ci} is used on a \code{fit} of
-#' class \code{glmerMod}.
+#' This function is one of the methods for \code{add_pi}, and is
+#' called automatically when \code{add_pi} is used on a \code{fit} of
+#' class \code{glmerMod}. 
 #'
-#' 
 #' @param tb A tibble or data frame of new data.
 #' @param fit An object of class \code{lmerMod}.
 #' @param alpha A real number between 0 and 1. Controls the confidence
@@ -56,21 +55,14 @@
 #'     \code{glmerMod} objects.
 #'
 #' @references
+#' TODO
 #'
 #' @examples
-#' # Define the data set
-#' dat <- lme4::sleepstudy
-#' # Fit a linear mixed model (random intercept model)
-#' fit <- lme4::lmer(Reaction ~ Days + (1|Subject), data = lme4::sleepstudy)
-#' # Get the fitted values for each observation in dat, and
-#' # append CIs for those fitted values to dat
-#' add_ci(dat, fit, alpha = 0.5)
-#' # Try the parametric bootstrap method, and make prediction at the population level
-#' add_ci(dat, fit, alpha = 0.5, type = "boot", includeRanef = FALSE, nSims = 100)
+#' TODO
 #'
 #' @export
 
-add_ci.glmerMod <- function(tb, fit, 
+add_pi.glmerMod <- function(tb, fit, 
                             alpha = 0.05, names = NULL, yhatName = "pred",
                             type = "boot", includeRanef = TRUE,
                             nSims = 200, ...){
@@ -83,20 +75,16 @@ add_ci.glmerMod <- function(tb, fit,
         names[2] <- paste("UCB", 1 - alpha/2, sep = "")
     }
     if ((names[1] %in% colnames(tb))) {
-        warning ("These CIs may have already been appended to your dataframe. Overwriting.")
+        warning ("These PIs may have already been appended to your dataframe. Overwriting.")
     }
 
     if (type == "boot")
-        bootstrap_ci_glmermod(tb, fit, alpha, names, includeRanef, nSims, yhatName)
+        bootstrap_pi_glmermod(tb, fit, alpha, names, includeRanef, nSims, yhatName)
     else
         stop("Incorrect type specified!")
 }
 
-ciTools_data <- new.env(parent = emptyenv())
-
-bootstrap_ci_glmermod <- function(tb, fit, alpha, names, includeRanef, nSims, yhatName) {
-
-    ciTools_data$tb_temp <- tb 
+bootstrap_pi_glmermod <- function(tb, fit, alpha, names, includeRanef, nSims, yhatName) {
 
     if (includeRanef) { 
         rform = NULL
@@ -106,15 +94,19 @@ bootstrap_ci_glmermod <- function(tb, fit, alpha, names, includeRanef, nSims, yh
         my_pred <- my_pred_fixed
     }
         
-    boot_obj <- lme4::bootMer(fit, my_pred, nsim=nSims, type="parametric", re.form = rform)
+    gg <- lme4::simulate(fit, my_pred, nsim=nSims, type="parametric", re.form = rform)
 
-    ci_out <- boot_quants(boot_obj, alpha) 
+    gg <- as.matrix(gg)
+    lwr <- apply(gg, 1, FUN = quantile, probs = alpha/2)
+    upr <- apply(gg, 1, FUN = quantile, probs = 1 - alpha / 2)
+
+    out <- predict(fit, tb, re.form = reform)
 
     if(is.null(tb[[yhatName]]))
-        tb[[yhatName]] <- ci_out$fit
-    tb[[names[1]]] <- ci_out$lwr
-    tb[[names[2]]] <- ci_out$upr
-    as_data_frame(tb)
-    
+        tb[[yhatName]] <- out
+    tb[[names[1]]] <- lwr
+    tb[[names[2]]] <- upr
+
+    tibble::as_data_frame(tb)
 }
 
